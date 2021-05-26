514d7f835debd512bd4b091dc7d65f06165233e3
Luis Fernando Leyva Luna <fernando_rendel892@hotmail.com>
2021-05-25 20:41:35 -0500

    .

--- ferdev/src/app/app-routing.module.ts
+++ ferdev/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'inicio', component: InicioComponent
+  },
+  {
+    path: 'servicios', component: ServiciosComponent
+  },
+  {
+    path: 'productos', component: ProductosComponent
+  },
+  {
+    path: 'portafolio', component: PortafolioComponent
+  },
+  {
+    path: '', pathMatch: 'full', redirectTo: 'inicio'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ferdev/src/app/app.module.ts
+++ ferdev/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ServiciosComponent,
+    ProductosComponent,
+    PortafolioComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
